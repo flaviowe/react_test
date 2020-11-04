c1e8fd99f0448d6bed3af2847ee02e0d73272331
Luis Flávio Carvalho Alves <flavio.alves@skyone.solutions>
2020-11-04 20:29:13 -0300

    add hookform resolvers

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,11 +1,9 @@
-import React, { useContext } from 'react'
-import { TodoContext } from '../contexts/TodoContext'
-import { TodoContextType } from '../contexts/TodoContextType'
-import { useForm } from 'react-hook-form'
-//import { yupResolver } from '@hookform/resolvers';
-import * as yup from 'yup'
-
-
+import React, { useContext } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+import { TodoContextType } from '../contexts/TodoContextType';
+import { useForm } from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/dist'
+import * as yup from 'yup';
 
 const schema = yup.object().shape(
     {
@@ -17,15 +15,34 @@ interface AddTodoForm {
     title: string
 }
 
-const AddTodo = () =>{
+const AddTodo = () => {
     const { addTodo } = useContext<TodoContextType>(TodoContext);
 
-    /*
-    const {register, handleSubmit, errors} = useForm({
+    const { register, handleSubmit, errors } = useForm({
         resolver: yupResolver(schema),
     });
-    */
-    return(<div>pano de chao</div>);
+
+    const onSubmit = (data: AddTodoForm, e: any) => {
+        addTodo(data.title);
+        e.target.reset();
+        window.location.href = '/';
+    }
+
+    return (
+        <form onSubmit={handleSubmit<AddTodoForm>(onSubmit)}>
+            <h4>Nova tarefa</h4>
+            <div className="uk-margin uk-width-1-1">
+                <input type="text" name="title" id="title" placeholder="Nova tarefa..." className="uk-input" ref={register} />
+                <span><small><strong className="uk-text-danger">{errors.title?.message}</strong></small></span>
+            </div>
+            <div className="uk-width-1-1">
+                <button type="submit" className="uk-button uk-button-primary">Salvar</button>
+            </div>
+        </form>
+
+    );
 }
+
+
 
 export default AddTodo;
